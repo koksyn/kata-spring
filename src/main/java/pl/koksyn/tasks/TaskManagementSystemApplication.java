3b0c30cdfecec9fad90e598b8700921602243754
Mateusz Palichleb <koksyn@pm.me>
2020-05-30 14:16:14 +0200

    Spring Boot init

---
+++ src/main/java/pl/koksyn/tasks/TaskManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.koksyn.tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/koksyn/tasks/TaskManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.koksyn.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
