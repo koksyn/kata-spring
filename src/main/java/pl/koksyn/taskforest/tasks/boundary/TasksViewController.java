d62f2b8e47992b700443ac0fc04d3343616bda53
Mateusz Palichleb <koksyn@pm.me>
2020-06-20 14:00:46 +0200

    Lesson: Handling Forms

--- src/main/java/pl/koksyn/taskforest/tasks/boundary/TasksViewController.java
+++ src/main/java/pl/koksyn/taskforest/tasks/boundary/TasksViewController.java
@@ -1,11 +1,16 @@
 package pl.koksyn.taskforest.tasks.boundary;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import pl.koksyn.taskforest.tasks.control.TasksService;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class TasksViewController {
@@ -14,7 +19,19 @@ public class TasksViewController {
     @GetMapping("/")
     public String hello(Model model) {
         model.addAttribute("tasks", tasksService.getAll());
-
+        model.addAttribute("newTask", new CreateTaskRequest());
         return "home";
+    }
+
+    @PostMapping("/tasks")
+    public String addTask(@ModelAttribute("newTask") CreateTaskRequest request) {
+        tasksService.addTask(request.getTitle(), request.getDescription(), request.getAuthor());
+        return "redirect:/";
+    }
+
+    @PostMapping("/tasks/delete/{id}")
+    public String deleteTask(@PathVariable Long id) {
+        tasksService.deleteTask(id);
+        return "redirect:/";
     }
 }
