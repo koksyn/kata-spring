5fdec4defa8cb47884d2acbc8ef1deb79391a154
Mateusz Palichleb <koksyn@pm.me>
2020-06-20 14:01:16 +0200

    Lesson: Handling Forms v2

--- src/main/java/pl/koksyn/taskforest/tasks/boundary/TasksViewController.java
+++ src/main/java/pl/koksyn/taskforest/tasks/boundary/TasksViewController.java
@@ -1,7 +1,6 @@
 package pl.koksyn.taskforest.tasks.boundary;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
